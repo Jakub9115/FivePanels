090f844bdc0f6543864793115b53489414033e4a
Jakub9115 <wac220786@spengergasse.at>
2024-06-12 22:57:19 +0200

    project init

---
+++ src/main/java/at/spengergasse/fivepanels/FivePanelsApplication.java
@@ -0,0 +1,13 @@
+package at.spengergasse.fivepanels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FivePanelsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FivePanelsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/spengergasse/fivepanels/FivePanelsApplicationTests.java
@@ -0,0 +1,13 @@
+package at.spengergasse.fivepanels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FivePanelsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
